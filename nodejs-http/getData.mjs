d25de889cdb6e2d8b5eabe7bdf67c822c046d13b
garold664 <garold664@gmail.com>
2024-06-16 22:02:35 +0300

    creates file which loads info from rest API server

---
+++ nodejs-http/getData.mjs
@@ -0,0 +1,14 @@
+import http from 'node:http';
+
+const url = 'http://jsonplaceholder.typicode.com/users/1';
+
+http.get(url, (res) => {
+  let data = '';
+  // res is instance of EventEmitter
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+});
