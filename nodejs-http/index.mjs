f23919a1dfb70efcf37fab0f5494b50c69529d9e
garold664 <garold664@gmail.com>
2024-06-18 21:16:37 +0300

    adds 404 response

--- nodejs-http/index.mjs
+++ nodejs-http/index.mjs
@@ -5,12 +5,13 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   // console.log(req);
 
-  if (req.url === '/http') {
+  if (req.url === '/html') {
     res.statusCode = 200;
     // res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Content-Type', 'text/html');
     res.write('<h1>Hello World</h1>');
-    return res.end('Hello World');
+    res.write('<p style="color: red">Hello World</p>');
+    return res.end();
   }
 
   if (req.url === '/json') {
@@ -18,6 +19,10 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'application/json');
     return res.end(JSON.stringify({ name: 'John', age: 30 }));
   }
+
+  res.statusCode = 404;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Page not found');
 });
 
 server.listen(PORT, () => {
